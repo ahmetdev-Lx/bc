3ec2dc9961e327e65148ea4531880dac19c5ebee
ahmetdev-Lx <137710648+ahmetdev-Lx@users.noreply.github.com>
2023-08-27 12:44:10 +0300

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -32,6 +32,6 @@ def get_expired_burses(site_url):
             })
     return burslar
 
-expired_burses = get_expired_burses("https://www.example.com/burslar")
+expired_burses = get_expired_burses("https://www.bursumcepte.com/")
 for burs in expired_burses:
     print(burs)
